3539e5d162b5e2b493d8cf9679dc13959e8c3667
KylianBalpe <balpamula@gmail.com>
2025-01-10 21:51:33 +0700

    chore: update theme provider import path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
